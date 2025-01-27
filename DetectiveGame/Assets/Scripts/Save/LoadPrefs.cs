2eab4a0f69c83dc1240d2fc92aabc4b3ce3da8a2
Kimera Royal <kai.anderson.ellis.brown@gmail.com>
2025-01-27 12:05:39 +0000

    v0.0.98: Attempting async scene loading

--- DetectiveGame/Assets/Scripts/Save/LoadPrefs.cs
+++ DetectiveGame/Assets/Scripts/Save/LoadPrefs.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using Save;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -16,14 +18,28 @@ public class LoadPrefs : MonoBehaviour
             return;
         }*/
 
-        void OnSceneLoaded(Scene _scene, LoadSceneMode _mode)
+        /*void OnSceneLoaded(Scene _scene, LoadSceneMode _mode)
         {
             SceneManager.sceneLoaded -= OnSceneLoaded;
             LoadInScene();
         }
 
-        SceneManager.sceneLoaded += OnSceneLoaded;
-        SceneManager.LoadScene(sceneName);
+        SceneManager.sceneLoaded += OnSceneLoaded;*/
+        StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    private IEnumerator LoadSceneAsync(string _sceneName)
+    {
+        var asyncLoad = SceneManager.LoadSceneAsync(_sceneName);
+        
+        if (asyncLoad == null) { yield break; }
+    
+        while (!asyncLoad.isDone)
+        { 
+            yield return null;
+        }
+        
+        LoadInScene();
     }
 
     private void LoadInScene()
